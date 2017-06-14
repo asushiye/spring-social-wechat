4d61802eec4c623b3fc225a7fa0a2c07077d4105
lazulite <junfu.chen@outlook.com>
2017-06-15 00:04:13 +0800

    修复微信oath url参数顺序

--- spring-social-wechat/src/main/java/org/springframework/social/wechat/connect/WeChatServiceProvider.java
+++ spring-social-wechat/src/main/java/org/springframework/social/wechat/connect/WeChatServiceProvider.java
@@ -30,6 +30,7 @@ public class WeChatServiceProvider extends AbstractOAuth2ServiceProvider<WeChat>
 	private static final String AUTHORIZE_URL = "https://open.weixin.qq.com/connect/qrconnect";
 
 	private static final String AUTHORIZE_PUBLIC_NUMBER_URL = "https://open.weixin.qq.com/connect/oauth2/authorize";
+	private static final String AUTHENTICATE_PUBLIC_NUMBER_URL = "https://open.weixin.qq.com/connect/oauth2/authorize";
 	private static final String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/sns/oauth2/access_token";
 
 	public WeChatServiceProvider(OAuth2Operations oauth2Operations) {
@@ -42,7 +43,7 @@ public class WeChatServiceProvider extends AbstractOAuth2ServiceProvider<WeChat>
 
 	private static OAuth2Template getOAuth2Template(String appId, String appSecret) {
 		OAuth2Template oAuth2Template = new WeChatOAuth2Template(appId, appSecret,
-				AUTHORIZE_PUBLIC_NUMBER_URL, ACCESS_TOKEN_URL);
+				AUTHORIZE_PUBLIC_NUMBER_URL,AUTHENTICATE_PUBLIC_NUMBER_URL, ACCESS_TOKEN_URL);
 		oAuth2Template.setUseParametersForClientAuthentication(true);
 		return oAuth2Template;
 	}
--- spring-social-wechat/src/main/java/org/springframework/social/wechat/oauth2/WeChatOAuth2Template.java
+++ spring-social-wechat/src/main/java/org/springframework/social/wechat/oauth2/WeChatOAuth2Template.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +19,7 @@ import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.social.oauth2.OAuth2Template;
 import org.springframework.social.support.FormMapHttpMessageConverter;
 import org.springframework.util.MultiValueMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 public class WeChatOAuth2Template extends OAuth2Template {
@@ -40,7 +42,101 @@ public class WeChatOAuth2Template extends OAuth2Template {
 		this.appid = clientId;
 		this.appsecret=clientSecret;
 	}
-	
+
+	public WeChatOAuth2Template(String clientId, String clientSecret, String authorizeUrl,String authenticateUrl, String accessTokenUrl) {
+		super(clientId, clientSecret, authorizeUrl ,authenticateUrl , accessTokenUrl);
+		try {
+			Field authorizeUrlField=OAuth2Template.class.getDeclaredField("authorizeUrl");
+			authorizeUrlField.setAccessible(true);
+			String authorizeUrlValue = (String) authorizeUrlField.get(this);
+			authorizeUrlField.set(this,authorizeUrlValue.replace("?client_id","?appid"));
+
+			Field authenticateUrlField=OAuth2Template.class.getDeclaredField("authenticateUrl");
+			authenticateUrlField.setAccessible(true);
+			String authenticateUrlValue = (String) authenticateUrlField.get(this);
+			authenticateUrlField.set(this,authenticateUrlValue.replace("?client_id","?appid"));
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		this.appid = clientId;
+		this.appsecret=clientSecret;
+	}
+
+	private String getAuthorizeUrl(){
+		Field authorizeUrlField= null;
+		String authorizeUrlValue =null;
+		try {
+			authorizeUrlField = OAuth2Template.class.getDeclaredField("authorizeUrl");
+			authorizeUrlField.setAccessible(true);
+			authorizeUrlValue = (String) authorizeUrlField.get(this);
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return authorizeUrlValue;
+
+	}
+
+	private String getAuthenticateUrl(){
+		Field authenticateUrlField= null;
+		String authenticateUrlValue =null;
+		try {
+			authenticateUrlField = OAuth2Template.class.getDeclaredField("authenticateUrl");
+			authenticateUrlField.setAccessible(true);
+			authenticateUrlValue = (String) authenticateUrlField.get(this);
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return authenticateUrlValue;
+
+	}
+
+	public String buildAuthenticateUrl(OAuth2Parameters parameters) {
+		return getAuthenticateUrl() != null ? buildAuthUrl(getAuthenticateUrl(), GrantType.AUTHORIZATION_CODE, parameters) : buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE, parameters);
+	}
+
+	public String buildAuthenticateUrl(GrantType grantType, OAuth2Parameters parameters) {
+		return getAuthenticateUrl() != null ? buildAuthUrl(getAuthenticateUrl(), grantType, parameters) : buildAuthorizeUrl(grantType, parameters);
+	}
+
+	public String buildAuthorizeUrl(OAuth2Parameters parameters) {
+		return buildAuthUrl(getAuthorizeUrl(), GrantType.AUTHORIZATION_CODE, parameters);
+	}
+
+	public String buildAuthorizeUrl(GrantType grantType, OAuth2Parameters parameters) {
+		return buildAuthUrl(getAuthorizeUrl(), grantType, parameters);
+	}
+
+
+	private String buildAuthUrl(String baseAuthUrl, GrantType grantType, OAuth2Parameters parameters) {
+		StringBuilder authUrl = new StringBuilder(baseAuthUrl);
+		List<String> redirectUri = parameters.remove("redirect_uri");
+		authUrl.append('&').append("redirect_uri").append('=').append(redirectUri.get(0));
+		if (grantType == GrantType.AUTHORIZATION_CODE) {
+			authUrl.append('&').append("response_type").append('=').append("code");
+		} else if (grantType == GrantType.IMPLICIT_GRANT) {
+			authUrl.append('&').append("response_type").append('=').append("token");
+		}
+		for (Iterator<Map.Entry<String, List<String>>> additionalParams = parameters.entrySet().iterator(); additionalParams.hasNext();) {
+			Map.Entry<String, List<String>> param = additionalParams.next();
+			String name = formEncode(param.getKey());
+			for (Iterator<String> values = param.getValue().iterator(); values.hasNext();) {
+				authUrl.append('&').append(name);
+				String value = values.next();
+				if (StringUtils.hasLength(value)) {
+					authUrl.append('=').append(formEncode(value));
+				}
+			}
+		}
+
+		return authUrl.toString();
+	}
+
 	protected AccessGrant createAccessGrant(String accessToken, String scope, String refreshToken, Long expiresIn, Map<String, Object> response) {
 		String openid = (String) response.get("openid");
 		return new WeChatAccessGrant(accessToken, scope, refreshToken, expiresIn, openid);
